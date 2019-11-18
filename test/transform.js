a2ac75144d5645463f9bee37f84824cd43cfd695
Owen Swerkstrom <owenswerk@gmail.com>
2019-11-18 14:44:52 -0600

    commented url-transform reference

--- test/transform.js
+++ test/transform.js
@@ -1,9 +1,8 @@
-let http = require("http");
-let url = require("url");
-
 let PORT = process.env.PORT || 4291;
 let DOMAIN = process.env.DOMAIN || "mydomain.net";
 
+let http = require("http");
+let url = require("url");
 http.createServer(function (req, res) {
 	let data = [];
 	req.on("data", function(chunk) {
@@ -17,8 +16,13 @@ http.createServer(function (req, res) {
 			res.end("400 Bad Request");
 			return;
 		} else {
-
 			let url_in = data.join("")
+
+			/*
+			  Here is the important part!
+			    - take in the posted URL
+				- transform and return it in a Location header
+			 */
 			let hostname_in = url.parse(url_in).hostname;
 			let transformed = url_in.replace(hostname_in, DOMAIN);
 
